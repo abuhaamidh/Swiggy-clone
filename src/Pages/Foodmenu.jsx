aaccf171366b63a99d7be4d7616b5dd895ddcb8f
Abu Haamidh <zapwap4@gmail.com>
2024-10-13 00:04:07 +0530

    Icons added o foodmenu

--- src/Pages/Foodmenu.jsx
+++ src/Pages/Foodmenu.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { FaSuitcase } from "react-icons/fa6";
 import { FiSearch } from "react-icons/fi";
 import { RiDiscountPercentLine } from "react-icons/ri";
+import { IoHelpBuoyOutline } from "react-icons/io5";
+import { BsPerson } from "react-icons/bs";
+import { BsMinecart } from "react-icons/bs";
 
 function Foodmenu() {
   return (
@@ -11,7 +14,9 @@ function Foodmenu() {
       <button class='btnn-1'><FaSuitcase style={{marginRight:'10px', marginTop:'-3px'}}/>Swiggy Corporate</button>
       <button class='btnn-2'><FiSearch style={{marginRight:'10px', marginTop:'-3px'}}/>Search</button>
       <button class='btnn-3'><RiDiscountPercentLine style={{marginRight:'10px', marginTop:'-3px'}}/>Offers</button>
-
+      <button class='btnn-4'><IoHelpBuoyOutline style={{marginRight:'10px', marginTop:'-3px'}}/>Help</button>
+      <button class='btnn-5'><BsPerson  style={{marginRight:'10px', marginTop:'-3px'}}/>Sign In</button>
+      <button class='btnn-6'><BsMinecart style={{marginRight:'10px', marginTop:'-3px'}}/>Cart</button>
       </div>
       
 
