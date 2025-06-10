83142f50023a189b57f41ffec1e815fb98163882
abuhaamidh <zapwap4@gmail.com>
2025-06-10 23:52:02 +0530

    updated the common header with styling

---
+++ src/components/CommonHeader.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { FaSuitcase } from "react-icons/fa6";
+import { FiSearch } from "react-icons/fi";
+import { RiDiscountPercentLine } from "react-icons/ri";
+import { IoHelpBuoyOutline } from "react-icons/io5";
+import { BsPerson } from "react-icons/bs";
+import { BsMinecart } from "react-icons/bs";
+
+function CommonHeader() {
+  return (
+
+
+<div className='boxx' style={{paddingTop: "1px" }}>
+      <div className='header-1'>
+      <img className = "logo-1" src= "https://miro.medium.com/v2/resize:fit:1000/1*TCc6vQVH-3EUiJea76pMbQ.png"></img>
+      <p id ="order">Orders</p>
+      <button className='btnn-1'><FaSuitcase style={{marginRight:'5px', marginTop:'-3px',  fontFamily:"'Courier New', Courier, monospace"}}/>Swiggy Corporate</button>
+      <button className='btnn-2'><FiSearch style={{marginRight:'5px', marginTop:'-3px'}}/>Search</button>
+      <button className='btnn-3'><RiDiscountPercentLine style={{marginRight:'5px', marginTop:'-3px'}}/>Offers</button>
+      <button className='btnn-4'><IoHelpBuoyOutline style={{marginRight:'10px', marginTop:'-3px'}}/>Help</button>
+      <button className='btnn-5'><BsPerson  style={{marginRight:'5px', marginTop:'-3px'}}/>Sign In</button>
+      <button className='btnn-6'><BsMinecart style={{marginRight:'5px', marginTop:'-3px'}}/>Cart</button>
+      </div>
+     <hr style={{marginTop:"80px", width:'85%', marginLeft:'6%'}}/>
+    <div style={{backgroundColor:"whitesmoke"}}>
+      abu
+    </div>
+     
+</div>
+    
+  )
+}
+
+export default CommonHeader
