287f483e861b27648bc7cceac2e878dbb563bd79
abuhaamidh <zapwap4@gmail.com>
2025-06-10 12:19:54 +0530

    Created an API using express for the json data

---
+++ server.cjs
@@ -0,0 +1,22 @@
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = 5000;
+
+app.use(cors());
+
+const products = require('./src/json/product.json');
+const cuisines = require('./src/json/cuisine.json');
+
+app.get('/api/product', (req, res) => {
+   res.json(products)
+});
+
+app.get('/api/cuisine', (req, res) => {
+    res.json(cuisines)
+});
+
+app.listen(PORT, () => {
+    console.log(`Woah! Your server is running is http://localhost${port}`);
+}) 
