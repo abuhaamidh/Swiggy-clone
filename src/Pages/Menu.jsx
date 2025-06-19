dd002abd7e4841662cbdb3c151ac1c3e974438a9
abuhaamidh <zapwap4@gmail.com>
2025-06-19 22:34:03 +0530

    Updated the menu page

---
+++ src/Pages/Menu.jsx
@@ -0,0 +1,105 @@
+import { useLocation } from "react-router-dom"
+import CommonHeader from "../components/CommonHeader"
+import Data from "../json/product.json"
+import { useEffect, useState, useRef} from "react"
+import { MdStars } from "react-icons/md";
+import OffersScroll from "../components/OffersScroll";
+import { DiAtom } from "react-icons/di";
+import { CiSearch } from "react-icons/ci";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+
+
+function Menudisplay(){
+
+    const [data, setData] = useState([]);
+    const { id } = useParams();
+    console.log(data, id)
+    const item = data.find((item) => String(item.id) === String(id));
+
+    function getMethod(){
+        return axios("http://localhost:5000/api/product")
+    }
+
+    useEffect(() => {
+
+        getMethod()
+            .then((res) => res.json) 
+            .then((data) => {
+             const found = data.find((item) => String(item.id) === String(id));
+            setData(found)
+            console.log('Filtered items:', found)
+            })
+            .catch((ërr) =>{
+             console.log("err")
+            })
+        
+        
+    },[id])
+   
+    
+    return (
+        <div>
+            <CommonHeader/>
+            <div style={{backgroundColor:"whitesmoke", height:'160rem'}}>
+                <p style= {{
+                    fontFamily:"'poppins', sans-serif",
+                    paddingLeft:'350px',
+                    paddingTop:'50px',
+                    fontWeight:'bold',
+                    fontSize:'13px',
+                    color:'grey'
+                    
+                    }}> Home / Restaurents / Menu </p>
+            {data && data
+            .map((e) => (
+            <div>
+           <h1 style={{fontFamily:"'poppins', sans-serif",fontSize:'26px', fontWeight:"bold", margin:'2rem 0 0 22rem'}}>{e.name}</h1>
+           
+            <div className="shopdetails">
+            <p style={{paddingLeft:'28px', paddingTop:'16px', fontWeight:'bold'}}> <MdStars style={{height:"25px", width:'25px', color:'green',paddingRight:'3px', paddingBottom:'3px'}}/>{e.rating} <span style={{color:'grey', paddingLeft:'5px', fontSize:'14px'}}> • {e.price}</span> </p>
+            <p style={{paddingLeft:'35px', marginTop:'-10px',fontWeight:'bold', fontSize:'14px', color:'#ff5200'}}>{e.cuisine}</p>
+           
+            <p style={{paddingLeft:'35px', marginTop:'-10px',fontWeight:'bold', fontSize:'14px'}}>Outlet <span style={{color:'grey',fontSize:'13px', paddingLeft:'12px', fontWeight:'lighter'}}>{e.location}</span></p>
+            <p style={{paddingLeft:'35px', marginTop:'-10px',fontWeight:'bold', fontSize:'14px'}}>25-30 mins</p>
+            </div>
+            </div>
+            ))}
+            <OffersScroll/>
+            <p style={{marginLeft:'44rem', marginTop:'50px'}}><DiAtom /> M E N U <DiAtom /></p>
+            <input className="searchdishes" placeholder="Search for dishes"></input>
+            <CiSearch style={{marginLeft:'-38px', color:'black'}}/>
+            <p className="menublack">MENU</p>
+            <p style={{border:'1px solid white', margin:'2rem 21rem', backgroundColor:'lightgrey',opacity:'30%', height:'20px'}}></p>
+            
+            {data && data.map((e) => (
+                <div className="mennuu">
+                <div className="displayMenu"> 
+                <h1 style={{fontSize:'19px',fontFamily:"'Outfit', sans-serif"}}>{e.name} </h1>
+                <hr style={{width:'8.4rem'}}></hr>
+                {data
+                
+                .menu.map((item) => (
+                    <div className='menu-section'style={{maxHeight:'15rem'}}>
+                    <p style={{fontWeight:'bold',fontFamily:"'Outfit', sans-serif"}}>{item.name}</p>
+                    <p style={{fontFamily:"'Outfit', sans-serif"}}>₹{item.price}</p>
+                    <button >ADD</button>
+                    <span id="customisable">Customisable</span>
+                    <p style={{fontFamily:"'poppins', sans-serif", width:"25rem", fontSize:'13px',marginTop:'-5rem'}}>{item.description}</p>
+                    <p style={{fontFamily:"'poppins', sans-serif", width:'80px',   fontSize:'13px' }}>{item.serving}</p>
+                    
+                    <hr/>
+                    
+                    </div>
+                ))}
+                </div> 
+                </div>   
+            ))}
+            
+            
+            </div>
+            </div>
+    )
+}
+
+export default Menudisplay
