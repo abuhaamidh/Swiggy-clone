067d69fe238ee07718c72940d1ce59f6a4d08232
abuhaamidh <zapwap4@gmail.com>
2025-06-20 14:51:23 +0530

    completed the menu page with useparam

--- src/Pages/Menu.jsx
+++ src/Pages/Menu.jsx
@@ -10,28 +10,27 @@ import axios from "axios";
 import { useParams } from "react-router-dom";
 
 
+
 function Menudisplay(){
 
     const [data, setData] = useState([]);
     const { id } = useParams();
-    console.log(data, id)
-    const item = data.find((item) => String(item.id) === String(id));
+    console.log('the restaurant id is:', id)
+    // const item = data.find((item) => String(item.id) === String(id));
 
-    function getMethod(){
-        return axios("http://localhost:5000/api/product")
-    }
+  
 
     useEffect(() => {
 
-        getMethod()
-            .then((res) => res.json) 
-            .then((data) => {
-             const found = data.find((item) => String(item.id) === String(id));
+             axios("http://localhost:5000/api/product")
+            .then((res) => {
+            const found = res.data.find((item) => String(item.id) === String(id));
             setData(found)
             console.log('Filtered items:', found)
+            
             })
             .catch((ërr) =>{
-             console.log("err")
+             console.log("error fetching data:", err)
             })
         
         
@@ -51,20 +50,20 @@ function Menudisplay(){
                     color:'grey'
                     
                     }}> Home / Restaurents / Menu </p>
-            {data && data
-            .map((e) => (
+            {data && (
+    
             <div>
-           <h1 style={{fontFamily:"'poppins', sans-serif",fontSize:'26px', fontWeight:"bold", margin:'2rem 0 0 22rem'}}>{e.name}</h1>
+           <h1 style={{fontFamily:"'poppins', sans-serif",fontSize:'26px', fontWeight:"bold", margin:'2rem 0 0 22rem'}}>{data.name}</h1>
            
             <div className="shopdetails">
-            <p style={{paddingLeft:'28px', paddingTop:'16px', fontWeight:'bold'}}> <MdStars style={{height:"25px", width:'25px', color:'green',paddingRight:'3px', paddingBottom:'3px'}}/>{e.rating} <span style={{color:'grey', paddingLeft:'5px', fontSize:'14px'}}> • {e.price}</span> </p>
-            <p style={{paddingLeft:'35px', marginTop:'-10px',fontWeight:'bold', fontSize:'14px', color:'#ff5200'}}>{e.cuisine}</p>
+            <p style={{paddingLeft:'28px', paddingTop:'16px', fontWeight:'bold'}}> <MdStars style={{height:"25px", width:'25px', color:'green',paddingRight:'3px', paddingBottom:'3px'}}/>{data.rating} <span style={{color:'grey', paddingLeft:'5px', fontSize:'14px'}}> • {data.price}</span> </p>
+            <p style={{paddingLeft:'35px', marginTop:'-10px',fontWeight:'bold', fontSize:'14px', color:'#ff5200'}}>{data.cuisine}</p>
            
-            <p style={{paddingLeft:'35px', marginTop:'-10px',fontWeight:'bold', fontSize:'14px'}}>Outlet <span style={{color:'grey',fontSize:'13px', paddingLeft:'12px', fontWeight:'lighter'}}>{e.location}</span></p>
+            <p style={{paddingLeft:'35px', marginTop:'-10px',fontWeight:'bold', fontSize:'14px'}}>Outlet <span style={{color:'grey',fontSize:'13px', paddingLeft:'12px', fontWeight:'lighter'}}>{data.location}</span></p>
             <p style={{paddingLeft:'35px', marginTop:'-10px',fontWeight:'bold', fontSize:'14px'}}>25-30 mins</p>
             </div>
             </div>
-            ))}
+            )}
             <OffersScroll/>
             <p style={{marginLeft:'44rem', marginTop:'50px'}}><DiAtom /> M E N U <DiAtom /></p>
             <input className="searchdishes" placeholder="Search for dishes"></input>
@@ -72,14 +71,14 @@ function Menudisplay(){
             <p className="menublack">MENU</p>
             <p style={{border:'1px solid white', margin:'2rem 21rem', backgroundColor:'lightgrey',opacity:'30%', height:'20px'}}></p>
             
-            {data && data.map((e) => (
+            {data &&  (
                 <div className="mennuu">
                 <div className="displayMenu"> 
-                <h1 style={{fontSize:'19px',fontFamily:"'Outfit', sans-serif"}}>{e.name} </h1>
+                <h1 style={{fontSize:'19px',fontFamily:"'Outfit', sans-serif"}}>{data.name} </h1>
                 <hr style={{width:'8.4rem'}}></hr>
-                {data
+                {
                 
-                .menu.map((item) => (
+                data && data.menu?.map((item) => (
                     <div className='menu-section'style={{maxHeight:'15rem'}}>
                     <p style={{fontWeight:'bold',fontFamily:"'Outfit', sans-serif"}}>{item.name}</p>
                     <p style={{fontFamily:"'Outfit', sans-serif"}}>₹{item.price}</p>
@@ -89,12 +88,11 @@ function Menudisplay(){
                     <p style={{fontFamily:"'poppins', sans-serif", width:'80px',   fontSize:'13px' }}>{item.serving}</p>
                     
                     <hr/>
-                    
                     </div>
-                ))}
+                ))} 
                 </div> 
                 </div>   
-            ))}
+            )}
             
             
             </div>
